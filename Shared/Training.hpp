b1cea4f652aa8ec74030d3d73fca5a9eac1d6c93
Linyi (Leo) Ni <nilinyi@gmail.com>
2019-03-10 23:00:13 -0700

    add more

---
+++ Shared/Training.hpp
@@ -0,0 +1,14 @@
+//
+//  Training.hpp
+//  ExTestApp
+//
+//  Created by Linyi Ni on 3/10/19.
+//  Copyright © 2019 Leo. All rights reserved.
+//
+
+#ifndef Training_hpp
+#define Training_hpp
+
+#include <stdio.h>
+
+#endif /* Training_hpp */
